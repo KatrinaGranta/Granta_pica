ec774393caba722702ee021c5563078b3b2c1588
Katrina <katrinagranta30@gmail.com>
2021-02-23 22:03:43 +0200

    No main switch daļa pievienota metode kura failā saglabā klienta informāciju

--- Granta_pica.java
+++ Granta_pica.java
@@ -1,4 +1,36 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
+
 public class granta_pica{
+static String failaNosaukums1;
+
+public static void klientaInfo(){
+		try{
+			FileWriter ryt = new FileWriter("pasutijums");
+			BufferedWriter out=new BufferedWriter(ryt);
+			JTextField vards = new JTextField();
+			JTextField adrese = new JTextField();
+			JTextField telefons = new JTextField();
+			Object[] message = {    "Klienta vards:", vards,"Klienta adrese:", adrese,"Klienta numurs:", telefons};
+			int answer = JOptionPane.showConfirmDialog(    null, message, "Ieraksti informaciju: ", JOptionPane.OK_CANCEL_OPTION);
+			if (answer == JOptionPane.OK_OPTION){    String klientaVards = vards.getText();
+			String KlientaAdrese = adrese.getText();
+			String TelNumurs = telefons.getText();
+			out.write("Klienta vards: " +vards.getText()+"\n");
+			out.write("Klienta adrese: " +adrese.getText()+"\n");
+			out.write("Klienta numurs: " +telefons.getText()+"\n");
+			out.close();
+			JOptionPane.showMessageDialog(null, "Informacija ir ierakstita!");
+			
+			}
+		}catch(Exception e){
+			JOptionPane.showMessageDialog(null, "Klume ierakstot faila!", "Klume!", JOptionPane.ERROR_MESSAGE);
+		}
+	}
+
 public static void main(String[] args) {  
 
 do {
@@ -8,15 +40,16 @@ do {
 		picasIzvele = picasIzvele.toLowerCase();
 switch(picasIzvele) {
 	case "1":
-break;
+	klientaInfo();
+	break;
 	case "2":
-break;
+
+	break;
 	case "3":
-break;
+	break;
 	case "x":
 		JOptionPane.showMessageDialog(null, "Programma aptureta!", "Bridinajums", JOptionPane.WARNING_MESSAGE);
 		break;
-		
 	default: 
  		JOptionPane.showMessageDialog(null, "Tada izvele nepastav!", "Klume!", JOptionPane.ERROR_MESSAGE);
  	break;
