c639d5654826a97045518ec1c7a3b51d08869fbb
Katrina <katrinagranta30@gmail.com>
2021-02-23 21:55:48 +0200

    Koda main daļā pievienoju switch izvēli

--- Granta_pica.java
+++ Granta_pica.java
@@ -6,6 +6,21 @@ do {
     		JOptionPane.showMessageDialog(null, "Edienu karte");
 		picasIzvele = JOptionPane.showInputDialog("1-Klienta info | 2-apskatit klienta info | 3-pasutit| x-close");
 		picasIzvele = picasIzvele.toLowerCase();
+switch(picasIzvele) {
+	case "1":
+break;
+	case "2":
+break;
+	case "3":
+break;
+	case "x":
+		JOptionPane.showMessageDialog(null, "Programma aptureta!", "Bridinajums", JOptionPane.WARNING_MESSAGE);
+		break;
+		
+	default: 
+ 		JOptionPane.showMessageDialog(null, "Tada izvele nepastav!", "Klume!", JOptionPane.ERROR_MESSAGE);
+ 	break;
+	}
 }while(!picasIzvele.equals("x"));
 }
 }
