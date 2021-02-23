dc70f2b21b7c66b72afa931c45a51caa3caa8ceb
Katrina <katrinagranta30@gmail.com>
2021-02-23 23:00:03 +0200

    Papildināts README fails

--- Granta_pica.java
+++ Granta_pica.java
@@ -7,36 +7,38 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class granta_pica{
-static String failaNosaukums1;
 
-public static void klientaInfo(){
+	static String failaNosaukums1;
+	
+	
+	public static void klientaInfo(){
 		try{
 			FileWriter ryt = new FileWriter("pasutijums");
 			BufferedWriter out=new BufferedWriter(ryt);
 			JTextField vards = new JTextField();
 			JTextField adrese = new JTextField();
 			JTextField telefons = new JTextField();
-			Object[] message = {    "Klienta vards:", vards,"Klienta adrese:", adrese,"Klienta numurs:", telefons};
-			int answer = JOptionPane.showConfirmDialog(    null, message, "Ieraksti informaciju: ", JOptionPane.OK_CANCEL_OPTION);
+			Object[] message = {    "Klienta vārds:", vards,"Klienta adrese:", adrese,"Klienta numurs:", telefons};
+			int answer = JOptionPane.showConfirmDialog(    null, message, "Ieraksti informāciju: ", JOptionPane.OK_CANCEL_OPTION);
 			if (answer == JOptionPane.OK_OPTION){    String klientaVards = vards.getText();
 			String KlientaAdrese = adrese.getText();
 			String TelNumurs = telefons.getText();
-			out.write("Klienta vards: " +vards.getText()+"\n");
+			out.write("Klienta vārds: " +vards.getText()+"\n");
 			out.write("Klienta adrese: " +adrese.getText()+"\n");
 			out.write("Klienta numurs: " +telefons.getText()+"\n");
 			out.close();
-			JOptionPane.showMessageDialog(null, "Informacija ir ierakstita!");
+			JOptionPane.showMessageDialog(null, "Informācija ir ierakstīta!");
 			
 			}
 		}catch(Exception e){
-			JOptionPane.showMessageDialog(null, "Klume ierakstot faila!", "Klume!", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
 		}
 	}
-public static void apskatit (){
+	public static void apskatit (){
 	
 		String teksts;
 		try{
-			failaNosaukums1 = JOptionPane.showInputDialog("pasutijums: ");
+			failaNosaukums1 = JOptionPane.showInputDialog("pasūtījums: ");
 			FileReader fr = new FileReader(failaNosaukums1);
 			BufferedReader lasa = new BufferedReader(fr);
 			while((teksts=lasa.readLine())!=null){
@@ -44,33 +46,71 @@ public static void apskatit (){
 			}
 			lasa.close();
 		}catch(Exception e){
-			JOptionPane.showMessageDialog(null, "Klume ierakstot faila!", "Klume!", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Kļūme ierakstot failā!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
 		}
 	}
-
-public static void main(String[] args) {  
-
-do {
-		
-    		JOptionPane.showMessageDialog(null, "Edienu karte");
-		picasIzvele = JOptionPane.showInputDialog("1-Klienta info | 2-apskatit klienta info | 3-pasutit| x-close");
+	
+	public static void main(String[] args) {  
+		String picasIzvele;
+	
+	do {
+		new granta_pica(); 
+    	JOptionPane.showMessageDialog(null, "Ēdienu karte");
+		picasIzvele = JOptionPane.showInputDialog("1-Klienta info | 2-apskatīt klienta info | 3-pasūtīt| x-close");
 		picasIzvele = picasIzvele.toLowerCase();
-switch(picasIzvele) {
+	switch(picasIzvele) {
 	case "1":
-	klientaInfo();
-	break;
-	case "2":
-	apskatit();
-	break;
-	case "3":
-	break;
-	case "x":
-		JOptionPane.showMessageDialog(null, "Programma aptureta!", "Bridinajums", JOptionPane.WARNING_MESSAGE);
+		klientaInfo();
 		break;
+	case "2":
+		apskatit();
+		break;
+	case "3":
+		String izvele, picasIzmers;
+		int klientaIzvele=0;
+		double rekins;
+		picasIzmers =  JOptionPane.showInputDialog(null, "Picas izmers: \n" + "1 - Lielā \n2 - Videjā" + "\n3 - mazā");
+		if(klientaIzvele ==1) {
+			rekins  =+ 9.99;
+		}else if(klientaIzvele == 2){
+			rekins =+ 5.99;
+		}else if(klientaIzvele == 3){
+			rekins =+ 3.99;
+			
+		}
+		izvele =  JOptionPane.showInputDialog(null, " Izvēlies savu picu: \n" + "1 - Kalifornijas pica \n2 - Siera pica" + "\n3 - Pikantā pica\n4 - Lauku pica");
+		klientaIzvele = Integer.parseInt(izvele);
+		if(klientaIzvele == 1 | klientaIzvele == 2);
+		rekins =+ 0;
+		
+		izvele = JOptionPane.showInputDialog(null,"Vai vēleis dzērienu?\n1 - Jā\n2- Nē");
+		klientaIzvele = Integer.parseInt(izvele);
+		 if(klientaIzvele == 1  )
+			 rekins += 0.90;
+		 
+		izvele = JOptionPane.showInputDialog(null,"Vai vēleis piedavas?\n1 - Jā\n2- Nē");
+			klientaIzvele = Integer.parseInt(izvele);
+			 if(klientaIzvele == 1  )
+				 rekins += 1.20;
+		 
+		izvele = JOptionPane.showInputDialog(null,"Vai pasūtījumu vajadzēs piegādāt?\n1 - Jā\n2- Nē");
+			klientaIzvele = Integer.parseInt(izvele);
+			 if(klientaIzvele == 1  )
+				 rekins += 3.00;
+		 
+		 JOptionPane.showMessageDialog(null,"Rēķins ir " + rekins );
+
+		
+		break;
+	case "x":
+		JOptionPane.showMessageDialog(null, "Programma apturēta!", "Brīdinājums", JOptionPane.WARNING_MESSAGE);
+		break;
+		
 	default: 
- 		JOptionPane.showMessageDialog(null, "Tada izvele nepastav!", "Klume!", JOptionPane.ERROR_MESSAGE);
+ 		JOptionPane.showMessageDialog(null, "Tāda izvēle nepastāv!", "Kļūme!", JOptionPane.ERROR_MESSAGE);
  	break;
 	}
-}while(!picasIzvele.equals("x"));
-}
+	}while(!picasIzvele.equals("x"));
+
+ }
 }
